f1089403c24cdd44b64394c7071d73fe6af0449f
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-19 17:01:32 +0700

    Array indexOf

---
+++ src/23-ArrayIndexOf.js
@@ -0,0 +1,15 @@
+/**
+ * Array IndexOf
+ */
+
+console.log([10, 20, 30, 40, 50].indexOf(30)); // 2
+console.log([{ name: 'Pam' }, { name: 'Kent' }].indexOf({ name: 'Kent' })); // -1
+console.log('hello world'.indexOf('o')); // 4
+console.log([[1], [2], [3], [4]].indexOf([2])); // -1
+
+
+const myArray = [5];
+const anotherArray = myArray;
+console.log([[1], [2], [3], [4], myArray].indexOf(anotherArray)); // 4
+
+
