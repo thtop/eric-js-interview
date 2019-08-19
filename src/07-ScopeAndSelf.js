3707d062773a2b555caab5a6c8b4774815e13691
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-19 10:01:40 +0700

    Scope and self

---
+++ src/07-ScopeAndSelf.js
@@ -0,0 +1,23 @@
+/**
+ * In Browser 
+ * 
+ * - Scope and 'self' 
+ * 
+ */
+
+this.color = 'red'; // Node.js = undefined
+
+var myCar = {
+    color: "Blue",
+    logColor: function () {
+        var self = this;
+        console.log("In logColor - this.color: " + this.color);
+        console.log("In logColor - self.color: " + self.color);
+        (function () {
+            console.log("In IIFE - this.color: " + this.color);
+            console.log("In IIFE - self.color: " + self.color);
+        })();
+    }
+};
+
+myCar.logColor();
